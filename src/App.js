7def3321622058fa204fd2c6caecf0c7a08d4ea6
Ivan Hattemer <ivanhattemer@gmail.com>
2021-07-21 12:40:42 +0300

    Add skeleton for app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from 'react'
+import Content from './components/Content'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import './index.css'
 
 function App() {
   return (
     <div>
-      App Placeholder
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Content} />
+          <Route path='/search' />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
